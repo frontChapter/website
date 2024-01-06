9e75859ecfc0a9494e845b0638b6bb779be02801
Ali Golkar <lokiwich@pm.me>
2024-01-06 17:25:18 +0330

    feat: :truck: fix database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,7 +20,6 @@ return new class extends Migration
             $table->string('password');
             $table->text('bio')->nullable();
             $table->enum('sex', ['unknown', 'woman', 'man', 'other'])->default('unknown');
-            $table->date('birth_date');
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
