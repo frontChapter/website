2d33ccc8513199cd09c2771fbe7f14bd6aeba3f4
Ali Golkar <lokiwich@pm.me>
2024-01-06 23:01:37 +0330

    fix: :bug: fix asset function call

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
             @if (trim($slot) === 'Laravel')
             <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
             @elseif (trim($slot) === 'FrontChapter')
-            <img src="{{ assets('images/logo.png') }}" class="logo" alt="Laravel Logo">
+            <img src="{{ asset('images/logo.png') }}" class="logo" alt="Laravel Logo">
             <div>{{ $slot }}</div>
             @else
             {{ $slot }}
