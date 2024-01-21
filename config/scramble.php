6d0fe8124f35575c4403256db32a02d7e95c8a46
Ali Golkar <lokiwich@pm.me>
2024-01-22 01:26:03 +0330

    fix: :bug: fix some bugs

--- config/scramble.php
+++ config/scramble.php
@@ -63,7 +63,7 @@ return [
      */
     'servers' => [
         'Localhost' => 'http://frontchapter.local/api',
-        'Production' => 'https://panlel.frontchapter.ir/api'
+        'Production' => 'https://panel.frontchapter.ir/api'
     ],
 
     'middleware' => [
