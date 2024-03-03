51908061c16346a4fd0ef2765205ca4b9a12405d
Ali Golkar <lokiwich@pm.me>
2024-03-03 17:51:47 +0330

    feat: :fix fix liara gift codes

--- app/Models/Gift.php
+++ app/Models/Gift.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Gift extends Model
 {
@@ -23,4 +24,14 @@ class Gift extends Model
         'link',
         'user_id',
     ];
+
+    /**
+     * Get the user that owns the site
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/livewire/user/gift/list-gifts.blade.php
+++ resources/views/livewire/user/gift/list-gifts.blade.php
@@ -6,15 +6,22 @@
     </x-slot>
 
     <div class="flex flex-col gap-4">
-        <h3 class="text-xl font-semibold leading-tight text-secondary-800 dark:text-secondary-200">
-            {{ __('Ticket Gifts') }}
-        </h3>
+        @foreach ($gifts as $gift)
+            <x-gift-card :href="$gift->link" :title="$gift->title" :description="$gift->description" :code="$gift->code" bgColor="bg-teal-600" />
+        @endforeach
         @foreach ($tickets as $ticket)
             <x-gift-card :title="__('Pachim')" :description="__('New experience in server and site management')" :code="$ticket->is_vip ? 'fchapter' : 'pchapter'" bgColor="bg-sky-600" />
             <x-gift-card :title="__('Roocket')" :description="__('Roocket, the story of the beginning of a programmer')" :code="$ticket->is_vip ? 'fchapter' : 'rchapter'" bgColor="bg-rose-600" />
-
-            <x-gift-card href="https://console.liara.ir/rewards" :title="__('Liara')" :description="__('In less than 5 minutes, run your program on the server')" code="{{ $ticket->code }}" bgColor="bg-teal-600" />
+            <x-gift-card expire="۱۱۴۰۲/۱۲/۱۹" href="https://console.liara.ir/rewards" :title="__('Liara')" :description="__('In less than 5 minutes, run your program on the server')" code="{{ $ticket->code }}" bgColor="bg-teal-600" />
         @endforeach
+
+        {{-- @if (count($gifts) === 0 && count($tickets) === 0) --}}
+        <x-empty-state :title="__('There are no gifts.')">
+            <x-slot name="icon">
+                <x-heroicon-s-gift class="w-24 h-24 mx-auto" />
+            </x-slot>
+        </x-empty-state>
+        {{-- @endif --}}
     </div>
 
 </x-profile-layout>
